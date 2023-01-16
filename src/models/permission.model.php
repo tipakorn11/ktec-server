87541ec5a5bc6a1eae25b2b13bf0a71364197101
tipakorn11 <55394461+tipakorn11@users.noreply.github.com>
2023-01-17 03:30:16 +0700

    permission fix

--- src/models/permission.model.php
+++ src/models/permission.model.php
@@ -18,7 +18,7 @@ class PermissionModel
                 IFNULL(permission_cancel, FALSE) AS permission_cancel,
                 IFNULL(permission_delete, FALSE) AS permission_delete
                 FROM permission as tb
-                LEFT JOIN menu ON menu.menuID = tb.menuID
+                LEFT JOIN menu ON menu.menuID = tb.menuID 
                 ORDER BY menu_group, tb.menuID;");
                                     
                 $sql->execute();
@@ -35,11 +35,18 @@ class PermissionModel
             $db = new db();
             $db = $db->connect();
            
-                $sql = $db->prepare("SELECT *,
-                                    ifnull((SELECT menu_name FROM menu WHERE menu.menuID = permission.menuID),permission.menuID)as menu_name,
-                                    ifnull((SELECT menu_group FROM menu WHERE menu.menuID = permission.menuID),permission.menuID)as menu_group 
-                                    FROM permission 
-                                    WHERE positionID = :position_id;");
+                $sql = $db->prepare("SELECT   
+                                    menu_group,
+                                    menu_name,
+                                    menu_name_en,
+                                    IFNULL(permission_view, FALSE) AS permission_view,
+                                    IFNULL(permission_add, FALSE) AS permission_add,
+                                    IFNULL(permission_edit, FALSE) AS permission_edit,
+                                    IFNULL(permission_approve, FALSE) AS permission_approve,
+                                    IFNULL(permission_cancel, FALSE) AS permission_cancel,
+                                    IFNULL(permission_delete, FALSE) AS permission_delete
+                                    FROM permission as tb
+                                    LEFT JOIN menu ON menu.menuID = tb.menuID AND positionID = :position_id || ''");
                 $sql->bindParam(':position_id',$data['positionID']);
                 $sql->execute();
                 $permission = $sql->fetchAll(PDO::FETCH_OBJ);
@@ -83,30 +90,41 @@ class PermissionModel
             return ['data'=>[],'require'=>false];
         }
     }
-    public function updatePermission($data)
-    {
+    public function updatePermission($data){
         try {
-
+            // Get DB Object
             $db = new db();
+            // connect to DB
             $db = $db->connect();
+            // query
             $sql = $db->prepare("UPDATE permission 
-                                SET permission_name = :pname 
-                                WHERE permissionID = :pid ; ");
-            $sql->bindParam(':pid', $data['permissionID']);
-            $sql->bindParam(':pname', $data['permission_name']);
+            SET permission_add = :permission_add,
+            permission_edit =:permission_edit,
+            permission_delete =:permission_delete,
+            permission_view =:permission_view
+            permission_approve =:permission_approve,
+            permission_cancel =:permission_cancel
+            WHERE position_id = :position_id AND menu_code =:menu_code;
+            ");
+            $sql->bindParam(':position_id',$data['position_id']);
+            $sql->bindParam(':menu_code',$data['menu_code']);
+            $sql->bindParam(':permission_add',$data['permission_add']);
+            $sql->bindParam(':permission_edit',$data['permission_edit']);
+            $sql->bindParam(':permission_delete',$data['permission_delete']);
+            $sql->bindParam(':permission_view',$data['permission_view']);
+            $sql->bindParam(':permission_approve',$data['permission_approve']);
+            $sql->bindParam(':permission_cancel',$data['permission_cancel']);
+
             $sql->execute();
             $db = null;
             if (!$sql) {
-
-                return ['data' => [], 'require' => false];
+                return ['data'=>[],'require'=>false];
             } else {
-
-                return ['data' => [], 'require' => true];
+                return ['data' =>[],'require'=>true];
             }
         } catch (PDOException $e) {
             $db = null;
-            echo $e->getMessage();
-            return ['data' => [], 'require' => false];
+            return ['data'=>[],'require'=>false];
         }
     }
    
